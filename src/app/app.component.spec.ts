90d1fab313bf0821fd986f519b7a4bbc939cf1b9
aalyabroude <abdullah.alyabroude@seracom.de>
2019-07-15 17:29:47 +0200

    create the basic files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-web'`, () => {
+  it(`should have as title 'my-third-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-web');
+    expect(app.title).toEqual('my-third-web');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-web!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-third-web!');
   });
 });
